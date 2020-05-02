0d9baabe35ee2d420493142a06a532a8b7b8d7ed
Tega Oboraruvwe <tegararuvwe@gmail.com>
2020-05-02 23:55:26 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <Header />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3"><QuestionBox /></b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+import QuestionBox from "./components/QuestionBox";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    QuestionBox,
+  },
+  data() {
+    return {
+      question: [],
+    };
+  },
+  mounted: function() {
+    fetch("https://opentdb.com/api.php?amount=10&category=27&type=multiple", {
+      method: "get",
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        this.question = data.results;
+        console.log(this.question, "tegas vue");
+      });
+  },
+};
+</script>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="wrap">
+    <b-jumbotron>
+      <template v-slot:lead>
+        Question here?
+      </template>
+
+      <hr class="my-4" />
+
+      <p>
+        List of answers
+      </p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<style>
+.wrap {
+  margin-top: 2em;
+}
+</style>
